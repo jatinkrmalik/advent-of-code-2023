2724a39835b5724c6b9a5d41291a8625d72f978d
Jatin K Malik <jmalik@atlassian.com>
2023-12-16 01:38:58 +0530

    Day 10 - Part 1 - Pythonic update

--- day_10/pipe_maze.py
+++ day_10/pipe_maze.py
@@ -1,8 +1,13 @@
-from collections import deque
+# -- Day 10: Pipe Maze ---
+
 from enum import Enum
 
 
 class Direction(Enum):
+    """
+    Enum representing cardinal directions with their grid movements.
+    """
+
     NORTH = (-1, 0)
     SOUTH = (1, 0)
     EAST = (0, 1)
@@ -16,7 +21,6 @@ REVERSE_DIRECTION_MAP = {
     Direction.WEST: Direction.EAST,
 }
 
-
 PIPE_MOVEMENT_OPTIONS = {
     "|": [Direction.NORTH, Direction.SOUTH],
     "-": [Direction.EAST, Direction.WEST],
@@ -28,118 +32,185 @@ PIPE_MOVEMENT_OPTIONS = {
 }
 
 
-def parse_maze(input_string):
-    return [list(line) for line in input_string.splitlines()]
+class Maze:
+    def __init__(self, input_string):
+        """
+        Initialize the Maze with a string representation.
+        :param input_string: Multiline string representing the maze.
+        """
+        self.maze = [list(line) for line in input_string.splitlines()]
+
+    def find_animal_start(self):
+        """
+        Find the starting position of the animal marked by 'S' in the maze.
+        :return: Tuple (x, y) as the starting position.
+        """
+        for i, row in enumerate(self.maze):
+            for j, cell in enumerate(row):
+                if cell == "S":
+                    return (i, j)
 
 
-def find_animal_start(maze):
-    for i, row in enumerate(maze):
-        for j, cell in enumerate(row):
-            if cell == "S":
-                return (i, j)
+class MazeSolver:
+    def __init__(self, maze):
+        """
+        Initialize the MazeSolver with the maze.
+        :param maze: 2D list representing the maze.
+        """
+        self.maze = maze
 
+    def find_cycle(self, start):
+        """
+        Find the cycle in the maze starting from the given position.
+        :param start: Tuple (x, y) as the starting position.
+        :return: List of tuples representing the cycle path.
+        """
+        stack = [start]
+        visited = {start}
+        path_tracker = {start: None}
 
-def find_cycle(start, maze):
-    stack = [start]
-    visited = {start}
-    path_tracker = {start: None}
+        while stack:
+            x, y = stack.pop()
+            for direction in PIPE_MOVEMENT_OPTIONS[self.maze[x][y]]:
+                nx, ny = x + direction.value[0], y + direction.value[1]
+                if self.can_move(x, y, nx, ny, direction):
+                    if (nx, ny) not in visited:
+                        self.update_stack_and_visited(
+                            stack, visited, path_tracker, x, y, nx, ny
+                        )
+                        break
+                    elif path_tracker[(x, y)] != (nx, ny):
+                        return self.construct_cycle(path_tracker, x, y, nx, ny)
+        raise ValueError("No cycle found")
 
-    while stack:
-        (x, y) = stack.pop()
+    def can_move(self, x, y, nx, ny, direction):
+        """
+        Check if movement is possible in the maze from (x, y) to (nx, ny).
+        :param x, y: Current position coordinates.
+        :param nx, ny: Next position coordinates.
+        :param direction: Direction of movement.
+        :return: Boolean indicating if movement is possible.
+        """
+        return (
+            0 <= nx < len(self.maze)
+            and 0 <= ny < len(self.maze[0])
+            and self.maze[nx][ny] in PIPE_MOVEMENT_OPTIONS
+            and REVERSE_DIRECTION_MAP[direction]
+            in PIPE_MOVEMENT_OPTIONS[self.maze[nx][ny]]
+        )
 
-        for direction in PIPE_MOVEMENT_OPTIONS[maze[x][y]]:
-            nx, ny = x + direction.value[0], y + direction.value[1]
-            if (
-                (0 <= nx < len(maze))
-                and (0 <= ny < len(maze[0]))
-                and (maze[nx][ny] in PIPE_MOVEMENT_OPTIONS)
-                and (REVERSE_DIRECTION_MAP[direction] in PIPE_MOVEMENT_OPTIONS[maze[nx][ny]])
-            ):
-                if (nx, ny) not in visited:
-                    stack.append((nx, ny))
-                    visited.add((nx, ny))
-                    path_tracker[(nx, ny)] = (x, y)
-                    break
+    def update_stack_and_visited(self, stack, visited, path_tracker, x, y, nx, ny):
+        """
+        Update the stack, visited set, and path tracker for the next move.
+        :param stack: List representing the stack of positions to explore.
+        :param visited: Set of visited positions.
+        :param path_tracker: Dictionary tracking the path taken.
+        :param x, y: Current position coordinates.
+        :param nx, ny: Next position coordinates.
+        """
+        stack.append((nx, ny))
+        visited.add((nx, ny))
+        path_tracker[(nx, ny)] = (x, y)
 
-                elif path_tracker[(x, y)] != (nx, ny):
-                    cycle = [(nx, ny)]  # We've found a cycle
-                    while (x, y) != (nx, ny):
-                        cycle.append((x, y))
-                        x, y = path_tracker[(x, y)]
-                    return cycle
+    def construct_cycle(self, path_tracker, x, y, nx, ny):
+        """
+        Construct the cycle path when a cycle is detected.
+        :param path_tracker: Dictionary tracking the path taken.
+        :param x, y: Current position coordinates.
+        :param nx, ny: Detected cycle starting position coordinates.
+        :return: List of tuples representing the cycle path.
+        """
+        cycle = [(nx, ny)]
+        while (x, y) != (nx, ny):
+            cycle.append((x, y))
+            x, y = path_tracker[(x, y)]
+        return cycle
 
-    # Raise error in case no cycle is found
-    raise ValueError("No cycle found")
-
-
-def get_steps_to_the_farthest_point(cycle):
-    return len(cycle) // 2
-
+    @staticmethod
+    def get_steps_to_the_farthest_point(cycle):
+        """
+        Calculate the number of steps to the farthest point in the cycle.
+        :param cycle: List of tuples representing the cycle path.
+        :return: Integer representing the number of steps.
+        """
+        return len(cycle) // 2
 
 
 def part_one():
+    # Read the maze input from a file
     with open("day_10/input.txt") as f:
-        maze = parse_maze(f.read())
-    start = find_animal_start(maze)
-    cycle = find_cycle(start, maze)
-    farthest_point = get_steps_to_the_farthest_point(cycle)
-    print(f"Steps to the farthest point: {farthest_point}")
+        maze_input = f.read()
+
+    # Initialize the Maze and MazeSolver
+    maze = Maze(maze_input)
+    solver = MazeSolver(maze.maze)
+
+    # Find the starting position and solve the maze
+    start = maze.find_animal_start()
+    cycle = solver.find_cycle(start)
+    farthest_point = solver.get_steps_to_the_farthest_point(cycle)
+
+    # Print the result
+    print(f"❗️ Steps to the farthest point: {farthest_point}")
 
 
 def test_simple_maze():
-    maze = """.....
+    maze_input = """.....
 .S-7.
 .|.|.
 .L-J.
 ....."""
-    maze = [row for row in maze.splitlines()]
-    start = find_animal_start(maze)
-    cycle = find_cycle(start, maze)
-    print(f"Cycle: {cycle}")
-    farthest_point = get_steps_to_the_farthest_point(cycle)
+
+    maze = Maze(maze_input)
+    solver = MazeSolver(maze.maze)
+    start = maze.find_animal_start()
+    cycle = solver.find_cycle(start)
+    farthest_point = solver.get_steps_to_the_farthest_point(cycle)
     assert farthest_point == 4, f"Expected 4, got {farthest_point}"
     print("✅ Simple maze test passed")
 
 
 def test_complex_maze():
-    maze = """-L|F7
+    maze_input = """-L|F7
 7S-7|
 L|7||
 -L-J|
 L|-JF"""
-    maze = parse_maze(maze)
-    start = find_animal_start(maze)
-    cycle = find_cycle(start, maze)
-    print(f"Cycle: {cycle}")
-    farthest_point = get_steps_to_the_farthest_point(cycle)
+    maze = Maze(maze_input)
+    solver = MazeSolver(maze.maze)
+    start = maze.find_animal_start()
+    cycle = solver.find_cycle(start)
+    farthest_point = solver.get_steps_to_the_farthest_point(cycle)
     assert farthest_point == 4, f"Expected 4, got {farthest_point}"
     print("✅ Complex maze test passed")
 
+
 def test_more_complex_maze():
-    maze = """..F7.
+    maze_input = """..F7.
 .FJ|.
 SJ.L7
 |F--J
 LJ..."""
-    maze = parse_maze(maze)
-    start = find_animal_start(maze)
-    cycle = find_cycle(start, maze)
-    print(f"Cycle: {cycle}")
-    farthest_point = get_steps_to_the_farthest_point(cycle)
+    maze = Maze(maze_input)
+    solver = MazeSolver(maze.maze)
+    start = maze.find_animal_start()
+    cycle = solver.find_cycle(start)
+    farthest_point = solver.get_steps_to_the_farthest_point(cycle)
     assert farthest_point == 8, f"Expected 8, got {farthest_point}"
     print("✅ More complex maze test passed")
 
+
 def test_most_complex_maze():
-    maze = """7-F7-
+    maze_input = """7-F7-
 .FJ|7
 SJLL7
 |F--J
 LJ.LJ"""
-    maze = parse_maze(maze)
-    start = find_animal_start(maze)
-    cycle = find_cycle(start, maze)
-    print(f"Cycle: {cycle}")
-    farthest_point = get_steps_to_the_farthest_point(cycle)
+    maze = Maze(maze_input)
+    solver = MazeSolver(maze.maze)
+    start = maze.find_animal_start()
+    cycle = solver.find_cycle(start)
+    farthest_point = solver.get_steps_to_the_farthest_point(cycle)
     assert farthest_point == 8, f"Expected 8, got {farthest_point}"
     print("✅ Most complex maze test passed")
 
