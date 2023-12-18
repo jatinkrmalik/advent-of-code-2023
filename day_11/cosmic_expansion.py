49f01452df0ed765cafd02795da7a22f2b5afb8b
Jatin K Malik <jmalik@atlassian.com>
2023-12-18 16:13:05 +0530

    Day 11 - Part 1 and 2 Optimised and Done

--- day_11/cosmic_expansion.py
+++ day_11/cosmic_expansion.py
@@ -1,146 +1,91 @@
 # Day 11: Cosmic Expansion
 
-from enum import Enum
-import heapq
+class CosmicGrid:
+    def __init__(self, space_grid):
+        # Initialize the cosmic grid with the provided space grid
+        self.space_grid = space_grid
+        self.grid_height = len(space_grid)
+        self.grid_width = len(space_grid[0])
+        # Find rows and columns that are entirely empty
+        self.empty_rows = self._find_empty_rows()
+        self.empty_columns = self._find_empty_columns()
+        # Locate all galaxies within the grid
+        self.galaxy_positions = self._find_galaxy_positions()
 
+    def _find_empty_rows(self):
+        # Identify rows that contain only empty space
+        empty_rows = set()
+        for row in range(self.grid_height):
+            if all(cell == '.' for cell in self.space_grid[row]):
+                empty_rows.add(row)
+        return empty_rows
 
-EMPTY_SPACE = "."
-GALAXY = "#"
-DISTANCE_MAP = {}
+    def _find_empty_columns(self):
+        # Identify columns that contain only empty space
+        empty_columns = set()
+        for col in range(self.grid_width):
+            if all(self.space_grid[row][col] == '.' for row in range(self.grid_height)):
+                empty_columns.add(col)
+        return empty_columns
 
+    def _find_galaxy_positions(self):
+        # Record the positions of all galaxies in the grid
+        galaxy_positions = []
+        for row in range(self.grid_height):
+            for col in range(self.grid_width):
+                if self.space_grid[row][col] == '#':
+                    galaxy_positions.append((row, col))
+        return galaxy_positions
 
-class Direction(Enum):
-    RIGHT = (0, 1)
-    LEFT = (0, -1)
-    DOWN = (1, 0)
-    UP = (-1, 0)
+    def find_shortest_distance(self, galaxy1, galaxy2, expansion_factor):
+        # Calculate the shortest distance between two galaxies, factoring in cosmic expansion
+        row_distance = 0
+        for r in range(min(galaxy1[0], galaxy2[0]), max(galaxy1[0], galaxy2[0]) + 1):
+            if r in self.empty_rows:
+                row_distance += 1
 
+        col_distance = 0
+        for c in range(min(galaxy1[1], galaxy2[1]), max(galaxy1[1], galaxy2[1]) + 1):
+            if c in self.empty_columns:
+                col_distance += 1
 
-def calculate_distance(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])  # Manhattan distance
+        # Expanded distance is calculated by multiplying empty distances by the expansion factor
+        # Direct distance is the straight line distance, not through empty space
+        expanded_row_distance = row_distance * expansion_factor
+        expanded_col_distance = col_distance * expansion_factor
+        direct_distance = abs(galaxy1[0] - galaxy2[0]) + abs(galaxy1[1] - galaxy2[1])
 
+        return expanded_row_distance + expanded_col_distance + direct_distance - row_distance - col_distance
 
-def get_adjacent_galaxies(space_map, galaxy):
-    adjacent_galaxies = []
-    for direction in Direction:
-        i, j = galaxy[0] + direction.value[0], galaxy[1] + direction.value[1]
-        if 0 <= i < len(space_map) and 0 <= j < len(space_map[0]):
-            adjacent_galaxies.append((i, j))
-    return adjacent_galaxies
+    def sum_of_shortest_paths(self, expansion_factor):
+        # Sum the shortest paths between all pairs of galaxies
+        total_distance = 0
+        for i, galaxy1 in enumerate(self.galaxy_positions):
+            for galaxy2 in self.galaxy_positions[i + 1:]:
+                total_distance += self.find_shortest_distance(galaxy1, galaxy2, expansion_factor)
+        return total_distance
 
-
-def find_shortest_path(space_map, start_galaxy, end_galaxy):
-    galaxies_to_visit = []
-    heapq.heappush(galaxies_to_visit, (0, start_galaxy))
-    previous_galaxy = {}
-    cost_to_reach = {start_galaxy: 0}
-    estimated_total_cost = {start_galaxy: calculate_distance(start_galaxy, end_galaxy)}
-
-    while galaxies_to_visit:
-        current_galaxy = heapq.heappop(galaxies_to_visit)[1]
-
-        if current_galaxy == end_galaxy:
-            return cost_to_reach[
-                current_galaxy
-            ]  # Return the cost to reach the end galaxy
-
-        for adjacent_galaxy in get_adjacent_galaxies(space_map, current_galaxy):
-            tentative_cost = cost_to_reach[current_galaxy] + 1  # assuming uniform cost
-            if (
-                adjacent_galaxy not in cost_to_reach
-                or tentative_cost < cost_to_reach[adjacent_galaxy]
-            ):
-                previous_galaxy[adjacent_galaxy] = current_galaxy
-                cost_to_reach[adjacent_galaxy] = tentative_cost
-                estimated_total_cost[
-                    adjacent_galaxy
-                ] = tentative_cost + calculate_distance(adjacent_galaxy, end_galaxy)
-                heapq.heappush(
-                    galaxies_to_visit,
-                    (estimated_total_cost[adjacent_galaxy], adjacent_galaxy),
-                )
-
-    raise Exception("No path found between the two galaxies")
-
-
-def expand_space_map(space_map):
-    empty_rows, empty_cols = [], []
-
-    for i, row in enumerate(space_map):
-        if GALAXY not in row:
-            empty_rows.append(i)
-
-    for i, col in enumerate(space_map[0]):
-        if GALAXY not in [row[i] for row in space_map]:
-            empty_cols.append(i)
-
-    mod_count = 0
-    for i in empty_rows:
-        space_map.insert(i + mod_count, EMPTY_SPACE * len(space_map[i]))
-        mod_count += 1
-
-    mod_count = 0
-    for i in empty_cols:
-        for j, row in enumerate(space_map):
-            space_map[j] = row[: i + mod_count] + EMPTY_SPACE + row[i + mod_count :]
-        mod_count += 1
-
-    return space_map
-
-
-def find_shortest_distance(galaxy1, galaxy2, space_map):
-    print(f"Finding shortest distance between {galaxy1} and {galaxy2}")
-
-    distance = DISTANCE_MAP.get(galaxy1, {}).get(galaxy2) or DISTANCE_MAP.get(
-        galaxy2, {}
-    ).get(galaxy1)
-    if distance:
-        return distance
-
-    distance = find_shortest_path(space_map, galaxy1, galaxy2)  # Use A* for pathfinding
-
-    if galaxy1 not in DISTANCE_MAP:
-        DISTANCE_MAP[galaxy1] = {}
-    DISTANCE_MAP[galaxy1][galaxy2] = distance
-    return distance
-
-
-def find_all_galaxies(space_map):
-    galaxies = []
-    for i, row in enumerate(space_map):
-        for j, col in enumerate(row):
-            if col == GALAXY:
-                galaxies.append((i, j))
-    return galaxies
-
-
-def analyze_space(space_map):
-    expanded_space_map = expand_space_map(space_map)
-    print(
-        f"Expanded space map is {len(expanded_space_map)} x {len(expanded_space_map[0])}"
-    )
-    galaxies_locations = find_all_galaxies(expanded_space_map)
-    print(f"Number of galaxies: {len(galaxies_locations)}")
-
-    for galaxy1 in galaxies_locations:
-        for galaxy2 in galaxies_locations:
-            if galaxy1 != galaxy2:
-                find_shortest_distance(galaxy1, galaxy2, expanded_space_map)
-
-    sum = 0
-    for k, v in DISTANCE_MAP.items():
-        for k1, v1 in v.items():
-            sum += v1
-
-    return sum
 
 
 def part_one():
     with open("day_11/input.txt") as f:
         space_map = f.read().splitlines()
-    sum = analyze_space(space_map)
+
+    cosmic_grid = CosmicGrid(space_map)
+    sum = cosmic_grid.sum_of_shortest_paths(expansion_factor=2)
     print(
-        f"❗️ Sum of the lengths of the shortest path between every pair of galaxies: {sum}"
+        f"❗️ Sum of the lengths of the shortest path between every pair of galaxies for expansion_factor=2: {sum}"
+    )
+
+
+def part_two():
+    with open("day_11/input.txt") as f:
+        space_map = f.read().splitlines()
+
+    cosmic_grid = CosmicGrid(space_map)
+    sum = cosmic_grid.sum_of_shortest_paths(expansion_factor=1000000)
+    print(
+        f"‼️ Sum of the lengths of the shortest path between every pair of galaxies for expansion_factor=1000000: {sum}"
     )
 
 
@@ -155,12 +100,23 @@ def test_analyze_space():
 ..........
 .......#..
 #...#....."""
-
-    sum = analyze_space(space_map.splitlines())
+    cosmic_grid = CosmicGrid(space_map.splitlines())
+    # part one
+    sum = cosmic_grid.sum_of_shortest_paths(expansion_factor=2)
     assert sum == 374, f"Expected 374, got {sum}"
-    print("✅ Passed test_analyze_observation()")
+    print("✅ Passed test_analyze_observation() for expansion_factor=2")
+
+    # part two
+    sum = cosmic_grid.sum_of_shortest_paths(expansion_factor=10)
+    assert sum == 1030, f"Expected 1030, got {sum}"
+    print("✅ Passed test_analyze_observation() for expansion_factor=10")
+
+    sum = cosmic_grid.sum_of_shortest_paths(expansion_factor=100)
+    assert sum == 8410, f"Expected 8410, got {sum}"
+    print("✅ Passed test_analyze_observation() for expansion_factor=100")
 
 
 if __name__ == "__main__":
     test_analyze_space()
-    # part_one()
+    part_one()
+    part_two()
--- day_11/problem.md
+++ day_11/problem.md
@@ -89,10 +89,21 @@ For example, here is one of the shortest paths between galaxies 5 and 9:
 ```
 This path has length 9 because it takes a minimum of nine steps to get from galaxy 5 to galaxy 9 (the eight locations marked # plus the step onto galaxy 9 itself). Here are some other example shortest path lengths:
 
-Between galaxy 1 and galaxy 7: 15
-Between galaxy 3 and galaxy 6: 17
-Between galaxy 8 and galaxy 9: 5
-In this example, after expanding the universe, the sum of the shortest path between all 36 pairs of galaxies is 374.
+- Between galaxy 1 and galaxy 7: 15
+- Between galaxy 3 and galaxy 6: 17
+- Between galaxy 8 and galaxy 9: 5
+
+In this example, after expanding the universe, the sum of the shortest path between all `36` pairs of galaxies is `374`.
 
 Expand the universe, then find the length of the shortest path between every pair of galaxies. What is the sum of these lengths?
+
+## --- Part Two ---
+
+The galaxies are much older (and thus much farther apart) than the researcher initially estimated.
+
+Now, instead of the expansion you did before, make each empty row or column one million times larger. That is, each empty row should be replaced with `1000000` empty rows, and each empty column should be replaced with `1000000` empty columns.
+
+(In the example above, if each empty row or column were merely `10` times larger, the sum of the shortest paths between every pair of galaxies would be `1030`. If each empty row or column were merely `100` times larger, the sum of the shortest paths between every pair of galaxies would be `8410`. However, your universe will need to expand far beyond these values.)
+
+Starting with the same initial image, expand the universe according to these new rules, then find the length of the shortest path between every pair of galaxies. What is the sum of these lengths?
 
